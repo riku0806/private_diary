32c22f3b69932a08ecb6e3249dc8b02a4424a9f9
riku0806 <ksw2070033@stu.o-hara.ac.jp>
2021-09-06 12:28:57 +0900

    初期セットアップ

---
+++ diary/views.py
@@ -0,0 +1,11 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from django.views import generic
+from django.views.generic.base import TemplateView
+
+# Create your views here.
+def index(request):
+    return render(request,'diary/index.html')
+
+class IndexView(generic.TemplateView):
+    template_name="diary/index.html"
