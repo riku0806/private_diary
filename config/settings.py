e9f2fec3b92b5db126f6378618369031746c7c30
riku0806 <ksw2070033@stu.o-hara.ac.jp>
2021-09-13 12:31:20 +0900

    認証

--- config/settings.py
+++ config/settings.py
@@ -143,11 +143,16 @@ MESSAGE_TAGS = {
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
 SITE_ID = 1
-AUTHENTICATION_BACkENDS = (
-    'allauth.acount.auth_backends.AuthenticationBackend',
+AUTHENTICATION_BACKENDS = (
+    'allauth.account.auth_backends.AuthenticationBackend',
     'django.contrib.auth.backends.ModelBackend',
 )
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_EMAIL_REQUIRED = True
+LOGIN_REDIRECT_URL='diary:index'
+ACCOUNT_LOGOUT_REDIRECT_URL='account_login'
+ACCOUNT_LOGOUT_ON_GET=True
+ACCOUNT_EMAIL_SUBJECT_PREFIX=''
+DEFAULT_FROM_EMAIL='admin@example.com'
