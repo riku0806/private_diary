9f2550b910a8ea0a26d39c70ed876bde0b803010
riku0806 <ksw2070033@stu.o-hara.ac.jp>
2021-09-16 13:41:31 +0900

    1

--- diary/models.py
+++ diary/models.py
@@ -10,7 +10,7 @@ class Diary(models.Model):
         CustomUser, verbose_name='ユーザー', on_delete=models.PROTECT)
     title = models.TextField(verbose_name='タイトル', max_length=40)
     content = models.TextField(verbose_name='本文', blank=True, null=True)
-    photol = models.ImageField(verbose_name='写真1', blank=True, null=True)
+    photo1 = models.ImageField(verbose_name='写真1', blank=True, null=True)
     photo2 = models.ImageField(verbose_name='写真2', blank=True, null=True)
     photo3 = models.ImageField(verbose_name='写真3', blank=True, null=True)
     created_at = models.DateTimeField(verbose_name='作成日時', auto_now_add=True)
